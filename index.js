2a2dd530c5ddc8af35de8611e2a691aa393eacce
aasane kariuki <franciskariki053@gmail.com>
2024-04-17 13:18:19 +0300

    looping assignment done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(arr, message) {
+    let result = [];
+    for (let item in arr) {
+      result.push(`Thank you, ${arr[item]}, for the wonderful ${message} gift!`);
+    }
+    return result;
+  }
+  function countDown() {
+    let downCount = 11;
+    while (downCount > 0) {
+      downCount--;
+      console.log(downCount);
+    }
+  }
